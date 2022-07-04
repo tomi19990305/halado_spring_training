8c6f55aa1d3c6a413b821743c97b861ad2f16aa1
Tamás Tóth <toth.tamas@alerant.hu>
2022-07-04 16:01:17 +0200

    Webclient

--- async/src/main/java/empapp/ClientService.java
+++ async/src/main/java/empapp/ClientService.java
@@ -3,8 +3,14 @@ package empapp;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Service
 @Slf4j
@@ -17,18 +23,17 @@ public class ClientService {
     public void getStatus(long id){
         log.info("Get status");
 
-        //HTTP kérés a háttérrendszer felé
-
         try{
             Thread.sleep(5000);
         } catch (InterruptedException e){
             log.error("InterruptedException ",e);
         }
 
-        String status = "200";
-        publisher.publishEvent(new CallCompletedEvent(id, status));
+        HttpClient client = HttpClient.create().responseTimeout(Duration.ofSeconds(5));
+        WebClient.builder().clientConnector(new ReactorClientHttpConnector(client)).build()
+                .get().uri("https://training360.com")
+                .exchangeToMono(clientResponse -> Mono.just(clientResponse.statusCode().toString()))
+                .subscribe(status -> publisher.publishEvent(new CallCompletedEvent(id, status)));
 
-        log.info("getStatus end");
-        //Háttérrendszer hívása - URL lekérése
     }
 }
