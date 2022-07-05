f7a5ddfa5d12701b8357b458a7e27b44bd6935cc
Tamás Tóth <toth.tamas@alerant.hu>
2022-07-05 09:34:06 +0200

    Cache init

---
+++ cache/src/main/java/empapp/EmployeesApplication.java
@@ -0,0 +1,16 @@
+package empapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class EmployeesApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+}
